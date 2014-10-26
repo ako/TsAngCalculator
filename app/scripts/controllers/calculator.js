c5ecfb9887ce52b48e2d827f8f4cc4dbc4bee87e
Andrej Koelewijn <andrej@koelewijn.net>
2014-10-26 22:29:06 +0100

    Added ionic

--- app/scripts/controllers/calculator.js
+++ app/scripts/controllers/calculator.js
@@ -12,25 +12,25 @@ var test3App;
             $scope.add = function () {
                 var newVal = { value: new Big($scope.valA), operation: "+" };
                 $scope.values.unshift(newVal);
-                var resultValBig = $scope.values[0].value.plus($scope.values[1].value);
+                var resultValBig = $scope.values[1].value.plus($scope.values[0].value);
                 $scope.values.unshift({ value: resultValBig, operation: "=" });
             };
             $scope.sub = function () {
                 var newVal = { value: new Big($scope.valA), operation: "-" };
                 $scope.values.unshift(newVal);
-                var resultValBig = $scope.values[0].value.minus($scope.values[1].value);
+                var resultValBig = $scope.values[1].value.minus($scope.values[0].value);
                 $scope.values.unshift({ value: resultValBig, operation: "=" });
             };
             $scope.div = function () {
                 var newVal = { value: new Big($scope.valA), operation: "/" };
                 $scope.values.unshift(newVal);
-                var resultValBig = $scope.values[0].value.div($scope.values[1].value);
+                var resultValBig = $scope.values[1].value.div($scope.values[0].value);
                 $scope.values.unshift({ value: resultValBig, operation: "=" });
             };
             $scope.times = function () {
                 var newVal = { value: new Big($scope.valA), operation: "*" };
                 $scope.values.unshift(newVal);
-                var resultValBig = $scope.values[0].value.times($scope.values[1].value);
+                var resultValBig = $scope.values[1].value.times($scope.values[0].value);
                 $scope.values.unshift({ value: resultValBig, operation: "=" });
             };
             $scope.clear = function () {
