1313a0ca0e4420450c7860755ab5809a45cade69
Andrej Koelewijn <andrej@koelewijn.net>
2014-10-31 07:49:36 +0100

    Initial router code, non functional

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,23 @@
 var test3App;
 (function (_test3App) {
     "use strict";
+
     var test3App = angular.module("test3App", ["ionic"]).controller("calculatorCtrl", _test3App.CalculatorCtrl);
+
     console.log("initialized test3App: " + test3App);
+    test3App.config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider.state("home", { url: "/home", templateUrl: "views/home.html" }).state("reverse-polish", {
+            url: "#/calculator/reverse-polish",
+            views: { "menuContent@": { templateUrl: "views/reversepolish.html" } } }).state("basic", {
+            url: "/calculator/basic",
+            views: { "menuContent": { templateUrl: "views/basic.html" } } }).state("scientific", {
+            url: "/calculator/scientific",
+            views: { "menuContent": { templateUrl: "views/scientific.html" } } }).state("wolfram", {
+            url: "/calculator/wolfram",
+            views: { "menuContent": { templateUrl: "views/wolfram.html" } } }).state("wind", {
+            url: "/calculator/wind",
+            views: { "menuContent": { templateUrl: "views/wind.html" } } });
+        $urlRouterProvider.otherwise("/home");
+    });
 })(test3App || (test3App = {}));
 //# sourceMappingURL=app.js.map
--- readme.md
+++ readme.md
@@ -4,3 +4,39 @@ Calculator written using the following techs:
  * Phonegap
  * Ionic
  * Big.js for accurate decimal types.
+ 
+ node installed using nvm (/Users/Andrej/.nvm)
+ nvm install -g goes install /users/andrej/.nvm/...
+ Do not use "sudo nvm install"
+
+ phonegap create my-app
+ cd my-app
+ phonegap run ios
+ npm install grunt (LOCAL!!!, use grunt-cli to run)
+ yo angular (NO sass/compass)
+ grunt serve
+
+ cd .nvm
+ . ./nvm.sh
+ use 0.11
+
+ phonegap create test3
+ phonegap run ios
+
+ npm install typescript
+ npm install grunt-typescript
+
+ bower install DefinitelyTyped --save-dev
+
+ http://frontendmatters.com/getting-started-mobile-app-development-phonegap-yeoman-angularjs-ionic/
+ http://nikosbaxevanis.com/blog/2014/04/03/typescript-slash-grunt-slash-angular/
+ http://www.scottlogic.com/blog/2014/08/26/StrongTypingWithAngularJS.html
+ http://notebookheavy.com/2013/05/22/angularjs-and-typescript/
+ http://www.slideshare.net/rstropek/angular-js-27309265?related=1
+ http://appfoundry.be/blog/2014/08/12/automated-ionic-builds/
+
+ npm install tslint
+ npm install grunt-tslint --save-dev
+
+ bower install angular-ui-router
+ bower install ionic
